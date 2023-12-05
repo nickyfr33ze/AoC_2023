85db6520b114176e856ddf6ea11f6b9957f36865
Nick Friesen <nfriesen26@gmail.com>
2023-12-04 16:53:42 -0800

    Started Part 2

---
+++ Day1/part2.py
@@ -0,0 +1,4 @@
+# Advent of Code 2023 Day 1 Part 2!
+# HUGE thanks to gamerplant3 @ https://github.com/gamerplant3 who was a HUGE help with this!
+# I was stuck on this for a while, and he helped me out a lot! I highly recommend you check out his GitHub for help with these puzzles!
+
